74c445e137b54255e4f1a8f145be0e8a009dc269
jdominguez <john.dominguez@satrack.com>
2018-06-28 11:47:37 -0500

    cambio de nombres en .sql para funcionamiento en linux

--- KitBasicoAutomApp/src/main/resources/db/migration/V2__createAfiliadosTable.sql
+++ KitBasicoAutomApp/src/main/resources/db/migration/V2__createAfiliadosTable.sql
@@ -1,4 +1,4 @@
-CREATE TABLE afiliado (id_afiliado BIGINT PRIMARY KEY auto_increment, numero_identificacion VARCHAR(10), 
+CREATE TABLE afiliados (id_afiliado BIGINT PRIMARY KEY auto_increment, numero_identificacion VARCHAR(10), 
 nombre VARCHAR(50), 
 apellido VARCHAR (50), 
 telefono VARCHAR(30));
